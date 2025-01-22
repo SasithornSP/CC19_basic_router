f3c7f9612fdd3ae4fa338d81cdb3cfa9bc5679af
SasithornSP <S.pinpitak@gmail.com>
2025-01-22 13:35:57 +0700

    update hdlClick

--- src/Store/productStore.jsx
+++ src/Store/productStore.jsx
@@ -1,10 +1,35 @@
 import{create} from 'zustand'
 
-    const AomjungStore=()=>({
-        firstName:'Sasithorn',
-        lastName:'sp',
-        address:{street:'1234',zipcode:'567'}
-    })
+    const AomjungStore=(set)=>({
+        // สร้าง state ชื่อ firstName และกำหนดค่าเริ่มต้นเป็นชื่อ Aomjung
+        // firstName:'Sasithorn',
+        // lastName:'sp',
+        // address:{street:'1234',zipcode:'567'},
+        // Profile:['Aom','ssp'],
+        // Products:[
+        //     {id:1,title:'ASUS',price:50000},
+        //     {id:2,title:'MSI',price:40000}
+        // ],
+        product:[
+
+        ],
+        number:5,
+        actionIncrease:()=>{
+            //*setNumber2((state)=>({num:state.num+1}))
+        set((state)=>({number:state.number+1}))
+        },
+        acttionDecrease:()=>{
+            set((state)=>({number:state.number-1}))
+        },
+        acttionMulti:()=>{
+            set((state)=>({number:state.number*2}))
+        },
+        acttionDivit:()=>{
+            set((state)=>({number:state.number/2}))
+        },
+       
+    });
+
     const useAomjungStore=create(AomjungStore)
 
 export default useAomjungStore
--- src/pages/EasyZustand.jsx
+++ src/pages/EasyZustand.jsx
@@ -1,14 +1,43 @@
 import useAomjungStore from '../Store/productStore'
 
 function EasyZustand() {
+//การใช้งาน Zustand ในการเก็บข้อมูล
+    // const FirstName =useAomjungStore((state)=>state.firstName)
+    // const address =useAomjungStore((state)=>state.address)
+    // const Profile =useAomjungStore((state)=>state.Profile)
+    // const Products =useAomjungStore((state)=>state.Products)
+    const number=useAomjungStore((state)=>state.number)
+    const actionIncrease=useAomjungStore((state)=>state.actionIncrease)
+    const acttionDecrease=useAomjungStore((state)=>state.acttionDecrease)
+    const acttionMulti=useAomjungStore((state)=>state.acttionMulti)
+    const acttionDivit=useAomjungStore((state)=>state.acttionDivit)
 
-    const FirstName =useAomjungStore((state)=>state.firstName)
-    const address =useAomjungStore((state)=>state.address)
-
-    console.log(FirstName);
-    console.log(address);
+    const hdlClick=()=>{
+      acttionDecrease()
+    }
   return (
-    <div>EasyZustand</div>
+    <div>EasyZustand
+      {/* <p>{Profile[0]}</p>
+      <p>{address.zipcode}</p>
+      <p>{Products[1].title} price: {Products[1].price}</p> */}
+      <p className='text-8xl'>{number}</p>
+      <button 
+      onClick={actionIncrease} 
+      className='bg-green-500'>Increase</button>
+      <br />
+      <button 
+      onClick={hdlClick} 
+      className='bg-red-300'>Decrease</button>
+      <br />
+      <button
+      onClick={acttionMulti} 
+      className='bg-blue-400'>Multi</button>
+      <br />
+      <button
+      onClick={acttionDivit} 
+      className='bg-pink-300'>Divit</button>
+
+    </div>
   )
 }
 
